8d664de75fb9a2d1b30e91d8674b50b0e9f4710d
Tiuku <tiia.iire@edu.turkuamk.fi>
2021-01-17 14:44:56 +0200

    Tehtävä 5

--- readuntil0.py
+++ readuntil0.py
@@ -1,12 +1,41 @@
+def evennums(num):
+    if num % 2 == 0:
+        return "is even"
+    else:
+        return
+def dividedbythree(num):
+    if num > 0 and num % 3 == 0:
+        return "is divided by three"
+    else:
+        return
+
 def readnums():
     neg_count = 0
+    even_count = 0
+    divide = 0
     
     while True:
         num = int(input("Write number: "))
+        if evennums(num) == "is even":
+            even_count += 1
+        elif dividedbythree(num) == "is divided by three":
+            divide += 1
         if num < 0:
             neg_count += 1
         elif num == 0:
-            return neg_count
-            
+            print("Number of even integers: ", even_count)
+            print("Number of negative integers: ", neg_count)
+            print("Number of positive integers divided by three", divide)
+            return
 
-print("Number of negative integers: ", readnums())
+readnums()
+
+        
+
+
+
+
+    
+
+
+
