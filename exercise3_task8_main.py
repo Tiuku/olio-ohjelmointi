797ee72043618744a6b46522d6f599811f2bb0a7
Tiuku <tiiaiire@edu.turkuamk.fi>
2021-01-30 22:18:50 +0200

    Exercise 3 Task 8

---
+++ exercise3_task8_main.py
@@ -0,0 +1,14 @@
+import exercise3_task8
+
+def main():
+    my_phone = exercise3_task8.CellPhone()
+
+    my_phone.Manufact()
+    my_phone.Model()
+    my_phone.RetailPrice()
+
+    print("Manufacturer: ", my_phone.get_manufact())
+    print("Model Number: ", my_phone.get_model())
+    print("Retail price: ", my_phone.get_retailPrice())
+
+main()
