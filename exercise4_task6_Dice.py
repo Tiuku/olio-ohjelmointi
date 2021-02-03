17cdd1f720f842f66e063eab3070e9faf4a48830
Tiuku <tiiaiire@edu.turkuamk.fi>
2021-02-03 13:02:40 +0200

    Exercise 4 task 6

--- exercise4_task6_Dice.py
+++ exercise4_task6_Dice.py
@@ -1,54 +1,28 @@
 # File name: exercise4_task6_Dice.py
 # Author: Tiia Iire
-# Description: Class where we roll the Dice, tell its color and numbers color
+# Description: Class where we roll the Dice
 
 import random
 
 class Dice:
     def __init__(self):
-        self.roll = "1"
-        self.color = "Black"
-        self.numcolor = "White"
+        self.roll = 1
 
     def diceroll(self):
         if random.randint(0, 5) == 0:
-            self.roll = "1"
+            self.roll = 1
         elif random.randint(0, 5) == 1:
-            self.roll = "2"
+            self.roll = 2
         elif random.randint(0, 5) == 2:
-            self.roll = "3"
+            self.roll = 3
         elif random.randint(0, 5) == 3:
-            self.roll = "4"
+            self.roll = 4
         elif random.randint(0, 5) == 4:
-            self.roll = "5"
+            self.roll = 5
         else:
-            self.roll = "6"
+            self.roll = 6
 
-    def dicecolor(self):
-        if random.randint(0, 3) == 0:
-            self.color = "Black"
-        elif random.randint(0, 3) == 1:
-            self.color = "Pink"
-        elif random.randint(0, 3) == 2:
-            self.color = "Purple"
-        else:
-            self.color = "Blue"
-
-    def dicenumcolor(self):
-        if random.randint(0, 3) == 0:
-            self.numcolor = "White"
-        elif random.randint(0, 3) == 1:
-            self.numcolor = "Red"
-        elif random.randint(0, 3) == 2:
-            self.numcolor = "Green"
-        else:
-            self.numcolor = "Yellow"
-
+    
     def get_num(self):
         return self.roll
 
-    def get_dicecolor(self):
-        return self.color
-
-    def get_numcolor(self):
-        return self.numcolor
--- exercise4_task6_main.py
+++ exercise4_task6_main.py
@@ -1,52 +1,84 @@
 # File name: exercise4_task6_main.py
 # Author: Tiia Iire
 # Description: Main file for getting phone model, manufacturer, retailprice and ID
+# Program will also roll the dice and see which phone equals the dice number
+# it will print the details from the phone that equals dice
 
 import exercise4_task4_Class as phone
 import exercise4_task6_Dice as dice
 
 def main():
-    my_phone = phone.CellPhone()
+    my_phone1 = phone.CellPhone()
+    my_phone2 = phone.CellPhone()
+    my_phone3 = phone.CellPhone()
+    my_phone4 = phone.CellPhone()
+    my_phone5 = phone.CellPhone()
+    my_phone6 = phone.CellPhone()
     my_dice = dice.Dice()
 
     my_dice.diceroll()
 
     print("Dice number: ", my_dice.get_num())
 
-    my_phone.set_Manufact()
-    my_phone.set_Model()
-    my_phone.set_RetailPrice()
-    my_phone.set_ID()
-    my_phone.correct_id()
-    my_phone.get_ID()
+    my_phone1.set_Manufact()
+    my_phone1.set_Model()
+    my_phone1.set_RetailPrice()
+    my_phone1.set_ID()
+    my_phone1.correct_id()
+    my_phone1.get_ID()
 
-    if my_phone.get_ID() == my_dice.get_num():
-        print(my_phone)
-    else:
-        print("None")
 
-    my_phone.set_Manufact()
-    my_phone.set_Model()
-    my_phone.set_RetailPrice()
-    my_phone.set_ID()
-    my_phone.correct_id()
-    my_phone.get_ID()
-    
-    if my_phone.get_ID() == my_dice.get_num():
-        print(my_phone)
-    else:
-        print("None")
+    my_phone2.set_Manufact()
+    my_phone2.set_Model()
+    my_phone2.set_RetailPrice()
+    my_phone2.set_ID()
+    my_phone2.correct_id()
+    my_phone2.get_ID()
+
    
-    my_phone.set_Manufact()
-    my_phone.set_Model()
-    my_phone.set_RetailPrice()
-    my_phone.set_ID()
-    my_phone.correct_id()
-    my_phone.get_ID()
+    my_phone3.set_Manufact()
+    my_phone3.set_Model()
+    my_phone3.set_RetailPrice()
+    my_phone3.set_ID()
+    my_phone3.correct_id()
+    my_phone3.get_ID()
 
-    if my_phone.get_ID() == my_dice.get_num():
-        print(my_phone)
+
+    my_phone4.set_Manufact()
+    my_phone4.set_Model()
+    my_phone4.set_RetailPrice()
+    my_phone4.set_ID()
+    my_phone4.correct_id()
+    my_phone4.get_ID()
+
+
+    my_phone5.set_Manufact()
+    my_phone5.set_Model()
+    my_phone5.set_RetailPrice()
+    my_phone5.set_ID()
+    my_phone5.correct_id()
+    my_phone5.get_ID()
+
+
+    my_phone6.set_Manufact()
+    my_phone6.set_Model()
+    my_phone6.set_RetailPrice()
+    my_phone6.set_ID()
+    my_phone6.correct_id()
+    my_phone6.get_ID()
+
+
+    if my_phone1.get_ID() == my_dice.get_num():
+        print(my_phone1)
+    elif my_phone2.get_ID() == my_dice.get_num():
+        print(my_phone2)
+    elif my_phone3.get_ID() == my_dice.get_num():
+        print(my_phone3)
+    elif my_phone4.get_ID() == my_dice.get_num():
+        print(my_phone4)
+    elif my_phone5.get_ID() == my_dice.get_num():
+        print(my_phone5)
     else:
-        print("None")
+        print(my_phone6)
     
 main()
